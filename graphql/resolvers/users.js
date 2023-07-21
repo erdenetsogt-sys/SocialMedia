6412d27f676ada7ae56d0dcddebc41137168c6fc
erdenetsogt-sys <tsogt1225@gmail.com>
2023-07-21 19:16:38 +0900

    added login & register user for backend

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,94 @@
+const bycrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+// const User = require("../../models/User");
+const User = require("../../models/User");
+const SECRET_KEY = require("../../config.js").SECRET_KEY;
+const { UserInputError } = require("apollo-server");
+const { validateRegisterInput } = require("../../util/validators");
+const { validateLoginInput } = require("../../util/validators");
+
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    "new_random_secret_key",
+    { expiresIn: "1h" }
+  );
+};
+
+module.exports = {
+  Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+      const user = await User.findOne({ username });
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+      const match = await bycrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+      const token = generateToken(user);
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
+      //Validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+      //Make sure user doesn't already exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      }
+
+      //hash password and create an auth token
+      console.log("SECRET_KEY:", SECRET_KEY);
+
+      password = await bycrypt.hash(password, 12);
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+      console.log("res:", res);
+      const token = generateToken(res);
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
+    },
+  },
+};
---
+++ index.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+const { MONGODB } = require("./config.js");
+const resolvers = require("./graphql/resolvers/index.js");
+const typeDefs = require("./graphql/typeDefs.js");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
