0b42e1b03bd51e38385c793b71eef5a7702c9498
erdenetsogt-sys <tsogt1225@gmail.com>
2023-07-23 21:44:56 +0900

    like, comment button wow, display posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,59 @@
 import React from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+import { Grid } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
 
 function Home() {
-  return <div>Home</div>;
+  const { loading, data } = useQuery(FETCH_POST_QUERY);
+  const posts = data ? data.getPosts : [];
+
+  return (
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+
+      <Grid.Row>
+        <Grid.Column>
+          {loading ? (
+            <h1>Loading posts..</h1>
+          ) : (
+            posts &&
+            posts.map((post) => (
+              <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                <PostCard post={post} />
+              </Grid.Column>
+            ))
+          )}
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 }
+
+const FETCH_POST_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      username
+      createdAt
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+      likes {
+        id
+        username
+        createdAt
+      }
+      likeCount
+      commentCount
+    }
+  }
+`;
 
 export default Home;
